dd564c5063207eee593e717596994b109993162a
Kethas97 <81663996+Kethas97@users.noreply.github.com>
2022-10-31 11:39:54 -0700

    Delete employee-service directory

--- employee-service/src/main/java/com/sgic/defect/employee/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgic.defect.employee.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sgic.defect.employee.entities.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
--- employee-service/src/main/java/com/sgic/defect/employee/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.sgic.defect.employee.service;
-
-import com.sgic.defect.employee.dto.EmployeeDto;
-
-public interface EmployeeService {
-	void createEmployee(EmployeeDto employeeDto);
-}
